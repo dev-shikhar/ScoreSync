e0278a1c77249f16b5271ea5030b537c83f07f66
dev-shikhar <147062188+dev-shikhar@users.noreply.github.com>
2025-08-09 14:30:16 +0530

    Update socket.js

--- client/socket.js
+++ client/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 console.log(import.meta.env.VITE_BACKEND_URL);
-const socket = io(import.meta.env.VITE_BACKEND_URL.BACKEND_URL, {
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
   autoConnect: false,
 });
 
