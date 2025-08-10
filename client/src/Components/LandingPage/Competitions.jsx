90273d2545cd44f2e03eaa03112f483d5d2021f9
dev-shikhar <147062188+dev-shikhar@users.noreply.github.com>
2025-08-10 17:57:04 +0530

    Update Competitions.jsx

--- client/src/Components/LandingPage/Competitions.jsx
+++ client/src/Components/LandingPage/Competitions.jsx
@@ -13,7 +13,6 @@ const Competitions = () => {
     async function fetchCompName() {
       try {
         const url = `${import.meta.env.VITE_BACKEND_URL}/top-competitions`;
-        console.log(url);
         const response = await fetch(url);
         const data = await response.json();
         setCompList(data);
